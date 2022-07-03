deb340827ef50f9fd440a7e487157d0f530771db
yoshida <rsna6ce@aol.com>
2022-07-03 13:58:15 +0900

    added files.

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # domino_machine
+
+## Overview
+* This machine is a prototype machine that automatically places dominoes.
+* Made for practicing servo control by Arduino
+* The target domino is "MARUKA Colorful Domino P50"
+  * http://www.maruka.jp/toy/toys/detail/?t=1&id=322
+  * https://amazon.co.jp/dp/B01CVCGDN2/
+  * This Domino is cheap and of poor quality
+  * Must be adjusted to stand vertically by scraping domino burrs
+* Remote controller operation
+  * Center key: Start stop
+  * Up / Down key: Narrow / wide domino spacing
+  * Left / Right key: Turn left / right
+
+## Dependent libraries
+* IRremote
+  * Ver 3.7.1
+  * Installed using ArduinoIDE Library Manager
+* VarSpeed ​​Servo
+  * https://github.com/netlabtoolkit/VarSpeedServo
+  * Download zip and zip library installation using ArduinoIDE Library Manager
+
+## Hardware used
+* Arduino UNO (compatible)
+  * https://amazon.co.jp/dp/B081DY1H2F/
+* TOWER PRO SG90 x1
+  * https://amazon.co.jp/dp/B016FKJJ8M/
+* TOWER PRO SG90-HV x2
+  * https://amazon.co.jp/dp/B07N6JP2XK/
+  * 360 degree type
+* IR receiver VS1838B
+  * https://amazon.co.jp/dp/B09JK5XTF2/
+* AA batteries x4
+* VKLSVAN MT3608 DC-DC converter
+  * Voltage conversion from AA battery x4 to 5V
+  * https://amazon.co.jp/dp/B07SHLL77G/
+* uxcell O-ring
+  * Outer diameter 21 mm, inner diameter 14 mm, cross-sectional diameter 3.5 mm
+  * https://amazon.co.jp/dp/B07J4YHTN6/
+* M2x20 tapping screw x2
+* M3x8 screw x1
+* 3D printed model
+  * Download my design from Thingiverse
+
+## Arduino wiring
+| Arduino Pin | Device | Pin | Remarks |
+| --- | --- | --- | --- |
+| VIN | DC-DC | OUT+ | set voltage 5V or over |
+| GND | DC-DC | OUT- ||
+| 5V | VS1838B | VCC ||
+| GND | VS1838B | GND ||
+| 3 | VS1838B | OUT ||
+| 5V | SG90 | Vcc | for arm |
+| GND | SG90 | Ground | for arm |
+| 9 | SG90 | PWM | for arm |
+| 5V | SG90-HV | Vcc | for right wheel |
+| GND | SG90-HV | Ground | for right wheel |
+| 10 | SG90-HV | PWM | for right wheel |
+| 5V | SG90-HV | Vcc | for left wheel |
+| GND | SG90-HV | Ground | for left wheel |
+| 11 | SG90-HV | PWM | for left wheel |
---
+++ domino_machine.ino
@@ -0,0 +1,134 @@
+#include <IRremote.hpp> // for ver3.7.1
+#include <VarSpeedServo.h> 
+#include <avr/wdt.h>
+
+#define IR_PIN_NO 3 /* ir */
+#define A_PIN_NO 9  /* servo for arm */
+#define R_PIN_NO 10 /* servo for right wheel */
+#define L_PIN_NO 11 /* servo for left wheel */
+VarSpeedServo servo1_a;
+VarSpeedServo servo1_r;
+VarSpeedServo servo1_l;
+
+// trim for servo individual difference
+#define STEERING_TRIM 3
+
+// parameters
+#define OFFSET_STEERING_VALUE 1
+#define OFFSET_STEERING_MAX 50
+#define OFFSET_RUN_MAX 500
+#define OFFSET_RUN_VALUE 100
+#define DELAY_RUN_AFTER_ARM 200
+
+// set code ir remote controler
+#define IR_STARTSTOP 0xE51AF708
+#define IR_UP        0xE41BF708
+#define IR_DOWN      0xE619F708
+#define IR_RIGHT     0xE11EF708
+#define IR_LEFT      0xFD02F708
+
+enum run_state {
+    arm_swing,
+    run_start,
+    run_stop,
+    waiting,
+};
+
+bool machine_running = false;
+int offset_run = 0;
+int offset_steering = STEERING_TRIM;
+unsigned long next_millis = 0;
+run_state next_state = waiting;
+
+/*set speed from -100% to 100%*/
+void set_speed_percent(int l, int r) {
+    // convert from percent to angle for servo
+    int l_angle = ((l * 90) / 100) + 90;
+    int r_angle = -((r * 90) / 100) + 90;
+    servo1_l.write(l_angle);
+    servo1_r.write(r_angle);
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+        String sdata = Serial.readStringUntil('\n');
+        int idata = sdata.toInt();
+        if (sdata=="") {
+            machine_running = !machine_running;
+        } else if (sdata=="a") {
+            // debug arm
+            servo1_a.write(50, 50, true);
+            servo1_a.write(0, 100, true);
+        } else if (sdata=="r") {
+            // debug run
+            set_speed_percent(25, 25);
+            delay(600);
+            set_speed_percent(0, 0);
+        }
+    }
+
+    if (IrReceiver.decode()) {
+        Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+        uint32_t decoded = IrReceiver.decodedIRData.decodedRawData;
+        if (decoded == IR_STARTSTOP) {
+            machine_running = !machine_running;
+        } else if (decoded == IR_UP && (offset_run <= OFFSET_RUN_MAX-OFFSET_RUN_VALUE)) {
+            offset_run += OFFSET_RUN_VALUE;
+        } else if (decoded == IR_DOWN && (OFFSET_RUN_VALUE-OFFSET_RUN_MAX <= offset_run)) {
+            offset_run -= OFFSET_RUN_VALUE;
+        } else if (decoded == IR_RIGHT && (offset_steering <= OFFSET_STEERING_MAX-OFFSET_STEERING_VALUE)) {
+            offset_steering += OFFSET_STEERING_VALUE;
+        } else if (decoded == IR_LEFT && (OFFSET_STEERING_VALUE-OFFSET_STEERING_MAX <= offset_steering)) {
+            offset_steering -= OFFSET_STEERING_VALUE;
+        }
+        delay(100);
+        IrReceiver.resume();
+    }
+
+    if (millis() > next_millis) {
+        if (next_state == arm_swing) {
+            // arm
+            servo1_a.write(50, 50, true);
+            servo1_a.write(0, 100, true);
+            next_state = run_start;
+            next_millis = millis() + 0; //nowait
+            
+        } else if (next_state == run_start) {
+            // run
+            set_speed_percent(25+offset_steering, 25-offset_steering);
+            next_state = run_stop;
+            next_millis = millis() + 600 + offset_run;
+            
+        } else if (next_state == run_stop) {
+            // stop
+            set_speed_percent(0, 0);
+            next_state = waiting;
+            next_millis = millis() + DELAY_RUN_AFTER_ARM;
+            
+        } else if (next_state == waiting) {
+            // waiting
+            if (machine_running) {
+                next_state = arm_swing;
+                next_millis = millis() + 0; //nowait
+            }
+        }
+    }
+    wdt_reset();
+}
+
+void setup() {
+    Serial.begin( 115200 );
+    Serial.println( "Hello Arduino!" );
+    pinMode(A_PIN_NO,INPUT);
+    pinMode(R_PIN_NO,INPUT);
+    pinMode(L_PIN_NO,INPUT);
+    servo1_a.attach(A_PIN_NO);
+    servo1_l.attach(L_PIN_NO);
+    servo1_r.attach(R_PIN_NO);
+    servo1_a.write(0);
+    set_speed_percent(0, 0);
+    IrReceiver.begin(IR_PIN_NO, ENABLE_LED_FEEDBACK);
+
+    wdt_disable();
+    wdt_enable(WDTO_2S);
+}
